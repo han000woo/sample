0216feb18edb8a22890fda2e5401414739a6aa4e
han000woo <gkstjsdn0011@naver.com>
2025-11-11 20:23:44 +0900

    directory remove

--- vue_template/vue-template/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router'; 
-
-const app = createApp(App);
-
-app.use(router); // 앱에 라우터 사용 등록
-app.mount('#app');
